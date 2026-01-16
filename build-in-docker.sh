72d7777a4ba60dd35248b6208217c50eb375d188
pedromchd <48498905+pedromchd@users.noreply.github.com>
2026-01-15 22:31:52 -0300

    fix: update volume paths in build-in-docker.sh for compatibility

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
+set -e
+
+if command -v pwd >/dev/null 2>&1 && pwd -W >/dev/null 2>&1; then
+  # Git Bash / Windows
+  HOST_PWD="$(pwd -W)"
+else
+  # Linux / WSL / macOS
+  HOST_PWD="$(pwd)"
+fi
 
 if [ -z "$1" ]; then
   docker pull jekyll/builder:pages
   docker run -it --rm \
     --workdir /srv/jekyll \
-    --volume="$PWD:/srv/jekyll:Z" \
-    --volume="$PWD/vendor/bundle:/usr/local/bundle:Z" \
+    --volume "$HOST_PWD:/srv/jekyll" \
+    --volume "$HOST_PWD/vendor/bundle:/usr/local/bundle" \
     --publish 4000:4000 \
     --publish 4001:4001 \
     jekyll/builder:pages \
